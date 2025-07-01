570c87e8f7f8c0907baf647a62d4f736b1fd8e66
melnikkovva <melnikovama555@gmail.com>
2025-07-01 10:20:16 +0300

    first

--- app/src/main/java/com/mobility/race/MainActivity.kt
+++ app/src/main/java/com/mobility/race/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello000000 $name!",
         modifier = modifier
     )
 }
