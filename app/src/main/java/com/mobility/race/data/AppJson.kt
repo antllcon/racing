76a89a4602dc686935174d6a974df9ea7251101a
antllcon <pokemonivan32@gmail.com>
2025-07-10 16:39:51 +0300

    Дописал реализацию Gateway.kt

---
+++ app/src/main/java/com/mobility/race/data/AppJson.kt
@@ -0,0 +1,42 @@
+package com.mobility.race.data
+
+import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketSession
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.modules.subclass
+
+val AppJson = Json {
+    encodeDefaults = true
+    ignoreUnknownKeys = true
+    prettyPrint = false
+    classDiscriminator = "kind"
+
+    serializersModule = SerializersModule {
+        polymorphic(ClientMessage::class) {
+            subclass(InitPlayerRequest::class)
+            subclass(CreateRoomRequest::class)
+            subclass(JoinRoomRequest::class)
+            subclass(LeaveRoomRequest::class)
+            subclass(PlayerActionRequest::class)
+        }
+        polymorphic(ServerMessage::class) {
+            subclass(PlayerConnectedResponse::class)
+            subclass(PlayerDisconnectedResponse::class)
+            subclass(InfoResponse::class)
+            subclass(ErrorResponse::class)
+            subclass(RoomCreatedResponse::class)
+            subclass(JoinedRoomResponse::class)
+            subclass(LeftRoomResponse::class)
+            subclass(RoomUpdatedResponse::class)
+            subclass(PlayerActionResponse::class)
+        }
+    }
+}
+
+suspend fun sendToSession(session: WebSocketSession, message: ClientMessage) {
+    val jsonStr = AppJson.encodeToString(message)
+    session.send(Frame.Text(jsonStr))
+}
--- app/src/main/java/com/mobility/race/data/Gateway.kt
+++ app/src/main/java/com/mobility/race/data/Gateway.kt
@@ -1,18 +1,16 @@
 package com.mobility.race.data
 
 import io.ktor.client.*
-import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.websocket.Frame
 import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.close
 import io.ktor.websocket.readText
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.ClosedReceiveChannelException
-import kotlinx.serialization.json.Json
 
 class Gateway(
     private val client: HttpClient,
-    private val json: Json,
     private val serverHost: String = "localhost",
     private val serverPort: Int = 8080,
     private val serverPath: String = "/",
@@ -23,12 +21,13 @@ class Gateway(
     private var job: Job? = null
 
     suspend fun connect() {
-        println("Gateway: Try connecting to ws://$serverHost:$serverPort$serverPath")
+        println("Gateway: Try connect to ws://$serverHost:$serverPort$serverPath...")
 
         try {
             session = setWebSocketSession()
             println("Gateway: Connected to WebSocket server")
             job = startGettingMessages(session!!)
+
         } catch (e: Exception) {
             onError("Failed to connect to server: ${e.message}")
             println("Gateway: Failed to connect to server")
@@ -36,31 +35,65 @@ class Gateway(
     }
 
     suspend fun disconnect() {
-        println("Gateway: try disconnecting")
+        println("Gateway: Try disconnect...")
+
+        try {
+            job?.cancelAndJoin()
+//            println("Gateway: Job cancelled and joined")
+            session?.close()
+//            println("Gateway: WebSocket session closed")
+
+        } catch (e: Exception) {
+            onError("Error during disconnect: ${e.message}")
+            println("Gateway: Error during disconnect")
+        } finally {
+            session = null
+            job = null
+            println("Gateway: Disconnected from WebSocket server")
+        }
     }
 
     private suspend fun sendMessage(message: ClientMessage) {
-        println("Gateway: trying to send message: $message")
+        println("Gateway: trying to send message: $message...")
+
+        if (session?.isActive == true) {
+            try {
+                sendToSession(session!!, message)
+                println("Gateway: Sent message")
+
+            } catch (e: Exception) {
+                onError("Failed to send message: ${e.message}")
+                println("Gateway: Failed to send message")
+            }
+        } else {
+            onError("Gateway is not connected or session is inactive. Cannot send message: $message")
+            println("Gateway: Not connected or session inactive, cannot send message")
+        }
     }
 
-    private fun initPlayer(name: String) {
+    suspend fun initPlayer(name: String) {
         println("Gateway: init player with name: $name")
+        sendMessage(InitPlayerRequest(name = name))
     }
 
     suspend fun createRoom(name: String) {
         println("Gateway: create room with name: $name")
+        sendMessage(CreateRoomRequest(name = name))
     }
 
     suspend fun joinRoom(name: String) {
         println("Gateway: join room with name: $name")
+        sendMessage(JoinRoomRequest(name = name))
     }
 
     suspend fun leaveRoom() {
-        println("Gateway: leave room")
+        println("Gateway: Sending LeaveRoomRequest.")
+        sendMessage(LeaveRoomRequest)
     }
 
     suspend fun playerAction(name: String) {
         println("Gateway: player action with name: $name")
+        sendMessage(JoinRoomRequest(name = name))
     }
 
     private suspend fun setWebSocketSession(): WebSocketSession {
@@ -91,7 +124,8 @@ class Gateway(
             val text = frame.readText()
 //            println("Gateway: get raw: $text")
             try {
-                val serverMessage = json.decodeFromString<ServerMessage>(text)
+                val serverMessage: ServerMessage =
+                    AppJson.decodeFromString<ServerMessage>(string = text)
                 onServerMessage(serverMessage)
             } catch (e: Exception) {
                 onError("Error Deserializing server message: ${e.message}")
