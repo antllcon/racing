b92782005578244f763664f906f8966485d22389
antllcon <pokemonivan32@gmail.com>
2025-07-15 15:51:36 +0300

    Создал интерфейс для Gateway

--- app/src/main/java/com/mobility/race/data/Gateway.kt
+++ app/src/main/java/com/mobility/race/data/Gateway.kt
@@ -1,5 +1,6 @@
 package com.mobility.race.data
 
+import com.mobility.race.Server
 import io.ktor.client.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.http.URLProtocol
@@ -12,60 +13,60 @@ import kotlinx.coroutines.channels.ClosedReceiveChannelException
 
 class Gateway(
     private val client: HttpClient,
-    private val serverHost: String = "thecorpus.ru",
-    private val serverPort: Int = 8080,
-    private val serverPath: String = "/",
+    private val serverConfig: Server,
     private val onServerMessage: (ServerMessage) -> Unit,
     private val onError: (String) -> Unit
-) {
+) : IGateway {
     private var session: WebSocketSession? = null
     private var job: Job? = null
 
-    suspend fun connect() {
-        println("Gateway: Try connect to wss://$serverHost:$serverPort$serverPath...")
+    override suspend fun connect() {
+        val protocolString = if (serverConfig.port == 443) "wss" else "ws"
+        println("Gateway: try connect to $protocolString://${serverConfig.host}:${serverConfig.port}${serverConfig.path}")
 
         try {
-            println("зашел сюда")
-            session = setWebSocketSession()
-            println("Gateway: Connected to WebSocket server")
+            session = client.webSocketSession(host = serverConfig.host, path = serverConfig.path) {
+                url {
+                    protocol = if (serverConfig.port == 443) URLProtocol.WSS else URLProtocol.WS
+                    port = serverConfig.port
+                }
+            }
             job = startGettingMessages(session!!)
 
         } catch (e: Exception) {
             onError("Failed to connect to server: ${e.message}")
-            println("Gateway: Failed to connect to server")
+            println("Gateway: fail to connect server")
         }
     }
 
-    suspend fun disconnect() {
-        println("Gateway: Try disconnect...")
+    override suspend fun disconnect() {
+        println("Gateway: try disconnect")
 
         try {
             job?.cancelAndJoin()
-//            println("Gateway: Job cancelled and joined")
             session?.close()
-//            println("Gateway: WebSocket session closed")
 
         } catch (e: Exception) {
             onError("Error during disconnect: ${e.message}")
             println("Gateway: Error during disconnect")
+
         } finally {
             session = null
             job = null
-            println("Gateway: Disconnected from WebSocket server")
+            println("Gateway: disconnect from server")
         }
     }
 
     private suspend fun sendMessage(message: ClientMessage) {
-        println("Gateway: trying to send message: $message...")
+        println("Gateway: try to send message: $message")
 
         if (session?.isActive == true) {
             try {
                 sendToSession(session!!, message)
-                println("Gateway: Sent message")
 
             } catch (e: Exception) {
                 onError("Failed to send message: ${e.message}")
-                println("Gateway: Failed to send message")
+                println("Gateway: fail to send message")
             }
         } else {
             onError("Gateway is not connected or session is inactive. Cannot send message: $message")
@@ -73,40 +74,29 @@ class Gateway(
         }
     }
 
-    suspend fun initPlayer(name: String) {
-        // Может использоваться для локальной инициализации игровой логики,
-        // которая не зависит от сетевого подключения
+    override suspend fun initPlayer(name: String) {
         println("Gateway: init player with name: $name")
         sendMessage(InitPlayerRequest(name = name))
     }
 
-    suspend fun createRoom(name: String) {
+    override suspend fun createRoom(name: String) {
         println("Gateway: create room with name: $name")
         sendMessage(CreateRoomRequest(name = name))
     }
 
-    suspend fun joinRoom(name: String) {
+    override suspend fun joinRoom(name: String) {
         println("Gateway: join room with name: $name")
         sendMessage(JoinRoomRequest(name = name))
     }
 
-    suspend fun leaveRoom() {
+    override suspend fun leaveRoom() {
         println("Gateway: Sending LeaveRoomRequest.")
         sendMessage(LeaveRoomRequest)
     }
 
-    suspend fun playerAction(name: String) {
+    override suspend fun playerAction(name: String) {
         println("Gateway: player action with name: $name")
         sendMessage(JoinRoomRequest(name = name))
-    }
-
-    private suspend fun setWebSocketSession(): WebSocketSession {
-        return client.webSocketSession(host = serverHost, path = serverPath) {
-            url {
-                protocol = URLProtocol.WSS
-                port = serverPort
-            }
-        }
     }
 
     private fun startGettingMessages(session: WebSocketSession): Job {
@@ -131,7 +121,6 @@ class Gateway(
     private fun processIncomingFrame(frame: Frame) {
         if (frame is Frame.Text) {
             val text = frame.readText()
-//            println("Gateway: get raw: $text")
             try {
                 val serverMessage: ServerMessage =
                     AppJson.decodeFromString<ServerMessage>(string = text)
