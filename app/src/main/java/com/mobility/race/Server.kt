24b19bdc094e00b6b1ad4427282ec9c778d1cbd4
antllcon <pokemonivan32@gmail.com>
2025-07-15 15:52:10 +0300

    Вынес данные для инициализации подклчения

---
+++ app/src/main/java/com/mobility/race/Server.kt
@@ -0,0 +1,22 @@
+package com.mobility.race
+
+data class Server(
+    val host: String,
+    val port: Int,
+    val path: String
+
+) {
+    companion object {
+        fun default() = Server(
+            host = "158.160.168.225",
+            port = 8080,
+            path = "/"
+        )
+
+        fun securityDefault() = Server(
+            host = "thecorpus.ru",
+            port = 443,
+            path = "/"
+        )
+    }
+}
