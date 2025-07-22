0a07a23ea3d3ffa011fadccaba937e4a9a7189ec
Gleb Ryzhov <glebka.r@yandex.ru>
2025-07-22 10:56:04 +0300

    Добавил ещё 3 машины

--- app/src/main/java/com/mobility/race/ui/drawUtils/BitmapStorage.kt
+++ app/src/main/java/com/mobility/race/ui/drawUtils/BitmapStorage.kt
@@ -19,6 +19,18 @@ fun bitmapStorage(): Map<String, ImageBitmap> {
         "car3_1" to ImageBitmap.imageResource(R.drawable.car3_1),
         "car3_2" to ImageBitmap.imageResource(R.drawable.car3_2),
         "car3_3" to ImageBitmap.imageResource(R.drawable.car3_3),
-        "car3_4" to ImageBitmap.imageResource(R.drawable.car3_4)
+        "car3_4" to ImageBitmap.imageResource(R.drawable.car3_4),
+        "car4_1" to ImageBitmap.imageResource(R.drawable.car4_1),
+        "car4_2" to ImageBitmap.imageResource(R.drawable.car4_2),
+        "car4_3" to ImageBitmap.imageResource(R.drawable.car4_3),
+        "car4_4" to ImageBitmap.imageResource(R.drawable.car4_4),
+        "car5_1" to ImageBitmap.imageResource(R.drawable.car5_1),
+        "car5_2" to ImageBitmap.imageResource(R.drawable.car5_2),
+        "car5_3" to ImageBitmap.imageResource(R.drawable.car5_3),
+        "car5_4" to ImageBitmap.imageResource(R.drawable.car5_4),
+        "car6_1" to ImageBitmap.imageResource(R.drawable.car6_1),
+        "car6_2" to ImageBitmap.imageResource(R.drawable.car6_2),
+        "car6_3" to ImageBitmap.imageResource(R.drawable.car6_3),
+        "car6_4" to ImageBitmap.imageResource(R.drawable.car6_4)
     )
 }
