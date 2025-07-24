dd6cd58a984502d93e2d6925bd05c6e541fa88a0
melnikkovva <melnikovama555@gmail.com>
2025-07-24 18:09:17 +0300

    исправлена мини карта

--- app/src/main/java/com/mobility/race/ui/drawUtils/GameMinimapComponent.kt
+++ app/src/main/java/com/mobility/race/ui/drawUtils/GameMinimapComponent.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import com.mobility.race.domain.Car
 import com.mobility.race.presentation.singleplayer.SingleplayerGameState
 import kotlin.math.min
 
