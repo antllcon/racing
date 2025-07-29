f0b5d6b1fa19003bc5c88f3b6265642a11c5393c
melnikkovva <melnikovama555@gmail.com>
2025-07-29 17:42:20 +0300

    dw

--- app/src/main/java/com/mobility/race/ui/drawUtils/GameMinimapComponent.kt
+++ app/src/main/java/com/mobility/race/ui/drawUtils/GameMinimapComponent.kt
@@ -6,17 +6,23 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import com.mobility.race.domain.Car
+import com.mobility.race.domain.CheckpointManager
+import com.mobility.race.domain.GameMap
 import com.mobility.race.presentation.singleplayer.SingleplayerGameState
 import kotlin.math.min
 
 fun DrawScope.drawMinimap(
-    state: SingleplayerGameState,
+    map: GameMap,
+    car: Car,
+    checkpointManager: CheckpointManager
 ) {
     val minimapSize = Size(300f, 300f)
     val minimapPosition = Offset(50f, 50f)
     val strokeWidth = 8f
     val strokeColor = Color.Gray
     val cornerRadius = 8f
+
     drawRoundRect(
         color = Color.Black.copy(alpha = 0.7f),
         topLeft = minimapPosition,
@@ -34,18 +40,18 @@ fun DrawScope.drawMinimap(
     )
 
     val cellSize = min(
-        innerMinimapSize.width / state.gameMap.width,
-        innerMinimapSize.height / state.gameMap.height
+        innerMinimapSize.width / map.width,
+        innerMinimapSize.height / map.height
     )
 
-    val nextCheckpoint = state.checkpointManager.getNextCheckpoint(state.car.id)
+    val nextCheckpoint = checkpointManager.getNextCheckpoint(car.id)
 
-    for (y in 0 until state.gameMap.height) {
-        for (x in 0 until state.gameMap.width) {
-            val terrain = state.gameMap.getTerrainType(x, y)
+    for (y in 0 until map.height) {
+        for (x in 0 until map.width) {
+            val terrain = map.getTerrainType(x, y)
             val color = when (terrain.uppercase()) {
                 "GRASS" -> Color(0xFF4CAF50)
-                "WATER" -> Color(0xFF2196F3)
+                "ABYSS" -> Color(0xFF2196F3)
                 "ROAD" -> Color(0xFF795548)
                 else -> Color.Gray
             }
@@ -80,8 +86,8 @@ fun DrawScope.drawMinimap(
     drawCircle(
         color = Color.Blue,
         center = Offset(
-            innerMinimapPosition.x + state.car.position.x * cellSize,
-            innerMinimapPosition.y + state.car.position.y * cellSize
+            innerMinimapPosition.x + car.position.x * cellSize,
+            innerMinimapPosition.y + car.position.y * cellSize
         ),
         radius = cellSize * 0.5f
     )
