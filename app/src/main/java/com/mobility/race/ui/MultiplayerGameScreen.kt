1a00121789bc56f44ffac18029de7549dddb72cb
antlllcon <pokemonivan32@gmail.com>
2025-08-01 17:08:32 +0300

    Исправил стик

--- app/src/main/java/com/mobility/race/ui/MultiplayerGameScreen.kt
+++ app/src/main/java/com/mobility/race/ui/MultiplayerGameScreen.kt
@@ -171,21 +171,22 @@ fun MultiplayerGameScreen(
 
             drawCars(state, bitmaps)
 
-            if (!state.mainPlayer.isFinished && state.countdown == 0) {
-                drawNextCheckpoint(
-                    state.checkpointManager.getNextCheckpoint(state.mainPlayer.car.id),
-                    state.gameCamera,
-                    state.gameCamera.getScaledCellSize(state.gameMap.size)
-                )
+            drawMinimap(state.gameMap, state.mainPlayer.car, viewModel.getCars(),state.mainPlayer.isFinished,state.checkpointManager)
 
+            if (!state.mainPlayer.isFinished && state.countdown == 0) {
                 drawControllingStick(
                     state.controllingStick,
                     currentStickInputAngle,
                     currentStickInputDistanceFactor
                 )
+
+                drawNextCheckpoint(
+                    state.checkpointManager.getNextCheckpoint(state.mainPlayer.car.id),
+                    state.gameCamera,
+                    state.gameCamera.getScaledCellSize(state.gameMap.size)
+                )
             }
 
-            drawMinimap(state.gameMap, state.mainPlayer.car, viewModel.getCars(),state.mainPlayer.isFinished,state.checkpointManager)
         }
 
         if (state.countdown > 0) {
